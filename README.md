30a8291637d4f72ccab44586931c10f03e19ab2d
Eduardo Hernández <eduardo@bor.re>
2019-04-29 16:15:41 -0500

    Making Redis module works

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # demofusionstage
 
-FusionStage Demo
+FusionStage Demo Project
--- src/main/java/com/eduardo/demoFusionStage/DemoFusionStageApplication.java
+++ src/main/java/com/eduardo/demoFusionStage/DemoFusionStageApplication.java
@@ -1,6 +1,5 @@
 package com.eduardo.demoFusionStage;
 
-
 import redis.clients.jedis.*;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -12,42 +11,46 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 @RestController
-public class DemoFusionStageApplication  {
+public class DemoFusionStageApplication {
 
     Logger logger = LoggerFactory.getLogger(DemoFusionStageApplication.class);
 
+    String prefix = System.getenv().getOrDefault("REDIS_URL", "bor.re");
+
     @RequestMapping("/")
     public String home() {
-        String prefix = System.getenv().getOrDefault("REDIS_URL", "bor.re");
         return "Hello Docker World " + prefix;
     }
 
     @RequestMapping("/redis")
     public String redisUrl() {
-        Long visits = null;
-        try {
-            Jedis jedis = new Jedis("172.168.140.134",15520);
-            jedis.connect();
-            jedis.auth("Huawei1234");
-            visits = jedis.incr("java_counter");
-            jedis.close();
-        } catch (Exception e) {
-            logger.error(e.toString());
+        if (!prefix.equals("bor.re")) {
+            Long visits = null;
+            try {
+                Jedis jedis = new Jedis(prefix);
+                visits = jedis.incr("java_counter");
+                jedis.close();
+            } catch (Exception e) {
+                logger.error(e.toString());
+            }
+
+            String hostname = "";
+            try {
+                hostname = InetAddress.getLocalHost().getHostName();
+            } catch (UnknownHostException e) {
+                logger.error(e.toString());
+            }
+            return "Hello from <br> host: <b>" + hostname + "</b> <br> number of visits: <b>" + visits.toString()
+                    + "</b>";
+        } else {
+            return "No  parameter provided to connection to Redis DB.";
         }
 
-        String hostname = "";
-        try {
-            hostname = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            logger.error(e.toString());
-        }
-        return "Hello from <br> host: <b>" + hostname + "</b> <br> number of visits: <b>" + visits + "</b>";
     }
 
-    @RequestMapping("/host") 
+    @RequestMapping("/host")
     public String restTest() {
         String hostname = "no hostname";
         try {
@@ -63,4 +66,3 @@ public class DemoFusionStageApplication  {
     }
 
 }
-
